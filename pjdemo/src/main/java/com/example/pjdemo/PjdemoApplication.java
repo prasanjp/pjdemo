6dd9f2c76b22eba54120f200cffad28433ea5bf5
Prasanna <Prasanna@localhost>
2020-06-28 13:58:11 -0700

    initial project

---
+++ pjdemo/src/main/java/com/example/pjdemo/PjdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.pjdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjdemoApplication.class, args);
+	}
+
+}
